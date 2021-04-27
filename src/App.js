4b3b8bdeda85d67b2afb7b51eaa89f8a6af6e70a
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-04-27 11:06:02 -0500

    installed react select and updated the app page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import Select from 'react-select';
+
+const options = [
+  { value: 'blues', label: 'Blues' },
+  { value: 'rock', label: 'Rock' },
+  { value: 'jazz', label: 'Jazz' },
+  { value: 'orchestra', label: 'Orchestra' } 
+];
 
 function App() {
   return (
     <div className="App">
-      
+      <Select options={options}/>
     </div>
   );
 }
